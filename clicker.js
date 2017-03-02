97f83a0a52d937d552b15ab9e215acea373d78c4
bigloof <xendesigns@gmail.com>
2017-03-01 23:31:54 -0700

    C l i c k e T
    Incremental Game

--- clicker.js
+++ clicker.js
@@ -1,59 +1,58 @@
-function Round(input,forcedecimal) { //Round a number to the "forcedecimal" number of decimal places
-	forcedecimal = Math.pow(10,forcedecimal); //Calculate the number of decimal places to round
- 
-	var result = Math.round(input * forcedecimal) / forcedecimal;
- 
-	return result;
+var SI_PREFIXES = ["", "K", "M", "B", "T", "P", "E"];
+
+function AbbreviateNumber(number){
+
+    // what tier? (determines SI prefix)
+    var tier = Math.log10(number) / 3 | 0;
+
+    // if zero, we don't need a prefix
+    if(tier == 0) return number;
+
+    // get prefix and determine scale
+    var prefix = SI_PREFIXES[tier];
+    var scale = Math.pow(10, tier * 3);
+
+    // scale the number
+    var scaled = number / scale;
+
+    // format number and add prefix as suffix
+    return scaled.toFixed(1) + prefix;
 }
-function AddSeparators(input,separator,forcedecimal) { //Add separators to large numbers, returns a string
-	var Before = String(input); //Turn the input into a string
-	var SplitBefore = Before.split("."); //Split the input by "." to get the decimal
-	var After = ""; //Reset the result
-	var LastThree = ""; //Reset the last 3 characters
- 
-	while (SplitBefore[0].length > 0) { //While the left half of the number is still there
-		LastThree = SplitBefore[0].slice(-3); //Take the last 3
-		SplitBefore[0] = SplitBefore[0].slice(0,-3); //Remove the last 3
-		if (After.length == 0) { After = LastThree; } else { After = LastThree + separator + After; } //Append the last 3
-	}
- 
-	if (forcedecimal > 0) { //If the decimal number is enabled
-		After += "."; //Add the decimal place
-		var AddZeros = forcedecimal;
- 
-		if (SplitBefore[1]) { //If a decimal string already exists
-			AddZeros -= SplitBefore[1].length; //Get the number of trailing zeros required
-			if (AddZeros < 0) {AddZeros = 0;}
-			After += SplitBefore[1].substring(0,forcedecimal - AddZeros); //Add the existing trailing digits
-		}
- 
-		for (var zeros=0;zeros<AddZeros;zeros++) { //Add any trailing zeros
-			After += "0";
-		}
-	}
- 
-	return After; //Return the number as a string
-} 
+
+
 var Dif = 12.75; //difficulty
 var coins = 0.0;
 var coinsPS = 0.0;
 var coinsPC = 1;
-var UpCost50 = 10;
+var UpCost50 = 50;
 var UpCost100 = 100;
 var UpCost1000 = 1000;
 var UpCostMil = 1000000; //1mil
 var UpCostBil = 1000000000; //1bil
 var UpCostTril = 1000000000000; //1tril
-    
+
+/*
+1,000,000,000,000,000,000,000,000	yotta	Y	Septillion	Quadrillion
+1,000,000,000,000,000,000,000	zetta	Z	Sextillion	Thousand Trillion/Trilliard
+1,000,000,000,000,000,000	exa	E	Quintillion	Trillion
+1,000,000,000,000,000	peta	P	Quadrillion	Thousand Billion/Billiard
+1,000,000,000,000	tera	T	Trillion	Billion
+1,000,000,000	giga	G	Billion	Thousand Million/Milliard
+1,000,000	mega	M	Million	Million
+1,000	kilo	k	Thousand	Thousand
+100	hecto	h	Hundred	Hundred
+10	deca	da	Ten	Ten>
+*/
+
 function gainCoin() {coins += coinsPC; }
-setInterval(function renderCoins() {document.getElementById("coins").innerHTML = "$" + AddSeparators(coins,","); });
-setInterval(function renderCoins() {document.getElementById("coinsPS").innerHTML = "$" + AddSeparators(coinsPS,',') + " /sec"; });
-setInterval(function renderCoins() {document.getElementById("UpCost50").innerHTML = "+ $" + UpCost50; });
-setInterval(function renderCoins() {document.getElementById("UpCost100").innerHTML = "+ $" + UpCost100; });
-setInterval(function renderCoins() {document.getElementById("UpCost1000").innerHTML = "+ $" + UpCost1000; });
-setInterval(function renderCoins() {document.getElementById("UpCostMil").innerHTML = "+ $" + UpCostMil; });
-setInterval(function renderCoins() {document.getElementById("UpCostBil").innerHTML = "+ $" + UpCostBil; });
-setInterval(function renderCoins() {document.getElementById("UpCostTril").innerHTML = "+ $" + UpCostTril; });
+setInterval(function renderCoins() {document.getElementById("coins").innerHTML = "$" + AbbreviateNumber(coins); });
+setInterval(function renderCoins() {document.getElementById("coinsPS").innerHTML = "$" + AbbreviateNumber(coinsPS) + " /sec"; });
+setInterval(function renderCoins() {document.getElementById("UpCost50").innerHTML = "+ $" + AbbreviateNumber(UpCost50); });
+setInterval(function renderCoins() {document.getElementById("UpCost100").innerHTML = "+ $" + AbbreviateNumber(UpCost100); });
+setInterval(function renderCoins() {document.getElementById("UpCost1000").innerHTML = "+ $" + AbbreviateNumber(UpCost1000); });
+setInterval(function renderCoins() {document.getElementById("UpCostMil").innerHTML = "+ $" + AbbreviateNumber(UpCostMil); });
+setInterval(function renderCoins() {document.getElementById("UpCostBil").innerHTML = "+ $" + AbbreviateNumber(UpCostBil); });
+setInterval(function renderCoins() {document.getElementById("UpCostTril").innerHTML = "+ $" + AbbreviateNumber(UpCostTril); });
 
 function gainFiftyPS() {
     if (coins >= UpCost50) {
@@ -145,4 +144,4 @@ setTimeout(function mouseUp() {
 },2000);
     }
 }
-setInterval(function coinPS() {coins += coinsPS; }, 2000);
+setInterval(function coinPS() {coins += coinsPS; }, 1000);
