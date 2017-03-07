28ac4a86e9e4fed7ca1e2383556931a46cb8a891
bigloof <xendesigns@gmail.com>
2017-03-06 18:58:37 -0700

    C l i c k e T
    incremental Game

--- clicker.js
+++ clicker.js
@@ -6,8 +6,7 @@ function AbbreviateNumber(number) {
 	var tier = Math.log10(number) / 3 | 0;
 
 	// if zero, we don't need a prefix
-	if (tier == 0)
-		return number;
+	if (tier == 0) return number;
 
 	// get prefix and determine scale
 	var prefix = SI_PREFIXES[tier];
@@ -20,17 +19,32 @@ function AbbreviateNumber(number) {
 	return scaled.toFixed(1) + prefix;
 }
 
-var Dif = 12.75; //difficulty
-var coins = 1.0;
-var coinsPS = 0.0;
+var Dif = 1; //difficulty 12.75
+var coins = 1;
+var coinsPS = 0;
 var coinsPC = 1;
 var UpCost1 = 1;
 var UpCost50 = 50;
 var UpCost100 = 100;
 var UpCost1000 = 1000;
-var UpCostMil = 1000000; //1mil
-var UpCostBil = 1000000000; //1bil
-var UpCostTril = 1000000000000; //1tril
+var UpCostMil = 1000000; //mega	M	Million	Million
+var UpCostBil = 1000000000; //giga	G	Billion	Thousand Million/Milliard
+var UpCostTril = 1000000000000; //tera	T	Trillion	Billion
+var UpCostQuad = 1000000000000000	; //peta	P	Quadrillion	Thousand Billion/Billiard
+var UpCostQuin = 1000000000000000000; //exa	E	Quintillion	Trillion
+var UpCostSext = 1000000000000000000000; //	zetta	Z	Sextillion	Thousand Trillion/Trilliard
+var UpCostSept = 1000000000000000000000000; //	yotta	Y	Septillion	Quadrillion
+var PS1 = 1;
+var PS50 = 50;
+var PS100 = 100;
+var PS1000 = 1000;
+var PSMil = 1000000; //mega	M	Million	Million
+var PSBil = 1000000000; //giga	G	Billion	Thousand Million/Milliard
+var PSTril = 1000000000000; //tera	T	Trillion	Billion
+var PSQuad = 1000000000000000	; //peta	P	Quadrillion	Thousand Billion/Billiard
+var PSQuin = 1000000000000000000; //exa	E	Quintillion	Trillion
+var PSSext = 1000000000000000000000; //	zetta	Z	Sextillion	Thousand Trillion/Trilliard
+var PSSept = 1000000000000000000000000; //	yotta	Y	Septillion	Quadrillion
 
 /*
  1,000,000,000,000,000,000,000,000	yotta	Y	Septillion	Quadrillion
@@ -83,13 +97,44 @@ setInterval(function renderCoins() {
 	document.getElementById("UpCostTril").innerHTML = "+ $"
 			+ AbbreviateNumber(UpCostTril);
 });
-
+setInterval(function renderMulti() {
+	document.getElementById("Multi").innerHTML = "Multiplier: x"
+			+ Multi;
+});
+setInterval(function renderNext() {
+	document.getElementById("Next").innerHTML = "Next Upgrade: Lvl "
+			+ Next;
+});
+var Next = 25;
+var Multi = 1;
+function Upgrade() {
+  if  (clicks == 24) {
+      coinsPS *= 2;
+      Multi *= 2;
+      Next += 25;
+      Dif *= 2;
+  } else if  (clicks == 49) {
+      coinsPS *= 2;
+      Multi += 2;
+      Next += 50;
+      Dif *= 2;
+  } else if  (clicks == 99) {
+      coinsPS *= 2;
+      Multi += 2;
+      Next += 150;
+      Dif *= 2;
+  }
+}
 function gainOnePS() {
 	if (coins >= UpCost1) {
 		coinsPS += 1;
 		coins -= UpCost1;
 		UpCost1 += 1 * Dif;
-	} else {
+        Upgrade();
+        drawslider(100, 0);
+        Click();
+       
+    } else {
 		setTimeout(function mouseDown() {
 			document.getElementById("color").style.color = "red";
 		});
@@ -192,3 +237,78 @@ function gainTrilPS() {
 setInterval(function coinPS() {
 	coins += coinsPS;
 }, 1000);
+
+var clicks = 0;
+    function Click() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+            
+    };
+setInterval(function renderClicks() {
+	document.getElementById("Lvl1").innerHTML = "Lvl. " + clicks;
+    });
+
+var slidewhere = 0;
+var holvanszlider = 0;
+function drawslider(ossz,meik){
+	var slidetext = Math.round((meik*100)/ossz);
+	document.getElementById("slidebar").style.width=slidetext+"%";
+	document.getElementById("slidetext").innerHTML=slidetext+"%";
+	holvanszlider = meik;
+	t = setTimeout("drawslider(100, slidewhere);slidewhere = holvanszlider + 1; if (slidewhere > 100) {slidewhere = 0;}",200);
+}
+
+/*
+///////////////////////////////////////////////////////////////////////////////////////////////
+//  Script by Paul Proft, http://proft.50megs.com/JSdemos.html  //
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Set timer lengths in HTML code for start buttons at bottom of this document.
+// Set desired container width and interval duration here:
+ 
+   containerWidth = 254;      // pixels (smaller widths draw smoother progress bars)
+   intervalDuration = 250;     // milliseconds (depending on computer speed, smaller interval durations might yield inaccurate progress)
+
+drawBar=0;
+function progressBar(duration) { 
+     resetStart();                                                                                                    // reset variables and style for button-click
+     ref=new Date();  start=ref.getTime();                                                                  // gets start time for accuracy check (optional) 
+     increment = intervalDuration*containerWidth/duration;                                         // container increment reference (pixels)
+     barWidth = interval*increment;                                                                          // bar incremental width (pixels)
+     drawBar = setInterval('progress('+duration%intervalDuration+')', intervalDuration);   // updates progress bar each interval duration
+}
+function progress(lastms) {
+     document.getElementById('bar').style.width=barWidth;                                                // updates bar width each iteration
+     document.getElementById('showsecs').innerHTML=Math.floor(interval*intervalDuration/1000);  // updates displayed seconds (optional)
+     interval++;                                                                                                                // increments interval for next iteration
+     if(interval*increment > containerWidth) {                                                                      // tests for last iteration
+          clearInterval(drawBar);                                                                                          // clears setInterval
+          end=new Date(); end=lastms+end.getTime();                                                         // gets end time for accuracy check (optional)
+          setTimeout("document.getElementById('bar').style.width=containerWidth",lastms);    // completes bar width and duration
+          setTimeout("document.getElementById('showsecs').innerHTML="+(lastms+intervalDuration*(interval-1))/1000, lastms); // final time
+          setTimeout('alert("Actual elapsed time: "+(end-start)/1000+" seconds")', lastms);      // bar duration accuracy check (optional)
+     }
+     else barWidth = interval*increment;                                                                            // bar width incremented in pixels
+}
+function resetStart() {                                                                                            // resets variables and style for button-click
+     if(drawBar) clearInterval(drawBar);                                                                     // clears setInterval if running
+     document.getElementById('bar').style.width = 0;                                                // sets bar width to zero
+     document.getElementById('showsecs').innerHTML = 0;                                      // sets seconds to zero
+     interval = 1;                                                                                                    // initializes interval 
+}
+//-->
+</script>
+</head>
+<body bgcolor=cornsilk topmargin=30>
+<h3 align=center>Javascript Timer Progress Bar Demo</h3>
+<table align=center cellspacing=0><tr><td>                                                             <!-- table used for centering container (optional) -->
+     <div id="container">                                                                                           <!-- container width defined in <head> script -->
+     <div id="bar"></div></div>                                                                                  <!-- bar width controlled by progressBar() -->
+     <div id="showsecs" style="text-align:center">&nbsp;</div>                                   <!-- container for showing seconds (optional) -->
+<script>document.getElementById('container').style.width=containerWidth</script>     <!-- show container when page loads (optional) -->
+</td></tr></table>                                                                               
+<p align=center>
+     <input type="button" value=" 5.375 seconds " onclick="progressBar(5375)">          <!-- set timer length (milliseconds) -->
+     <input type="button" value=" 30 seconds " onclick="progressBar(30000)">             <!-- set timer length (milliseconds) -->
+<br><br><font size=-1>(1 second = 1000 ms)</font></p>
+    */
